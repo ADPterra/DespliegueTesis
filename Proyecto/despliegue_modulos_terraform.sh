e615b281a20cc619dd52a2cc21239b5bdeacbbed
ADPterra <154098594+ADPterra@users.noreply.github.com>
2023-12-18 19:44:05 -0300

    Update despliegue_modulos_terraform.sh

--- Proyecto/despliegue_modulos_terraform.sh
+++ Proyecto/despliegue_modulos_terraform.sh
@@ -2,10 +2,14 @@ cd /home/Proyecto/modulos/Red
 terraform init
 terraform apply -auto-approve
 cd /home/Proyecto/modulos/SubRed
+terraform init
 terraform apply -auto-approve
 cd /home/Proyecto/modulos/Instancias
+terraform init
 terraform apply -auto-approve
 cd /home/Proyecto/modulos/Firewall
+terraform init
 terraform apply -auto-approve
 cd /home/Proyecto/modulos/Rutas
+terraform init
 terraform apply -auto-approve
